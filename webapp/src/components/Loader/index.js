5f2e54e4f6294ea0bff295e3f0d337d5883a8744
bbumjun <bumjun7928@gmail.com>
2021-06-21 14:48:12 +0900

    💄로더 컴포넌트 생성

---
+++ webapp/src/components/Loader/index.js
@@ -0,0 +1,9 @@
+import Component from "../../core/Component";
+import "./style";
+export default class Loader extends Component {
+  template() {
+    return `
+      <div class="loader"></div>
+    `;
+  }
+}
