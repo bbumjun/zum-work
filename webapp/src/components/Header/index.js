d8a6935455915cf27164c1e856c42fb32356e317
bbumjun <bumjun7928@gmail.com>
2021-06-21 14:48:51 +0900

    ✨ 헤더에서 메뉴바 NavList컴포넌트로 분리

--- webapp/src/components/Header/index.js
+++ webapp/src/components/Header/index.js
@@ -1,30 +1,38 @@
 import Component from "../../core/Component";
+import NavList from "../NavList";
 import "./style";
 export default class Header extends Component {
   template() {
+    const navItems = [
+      { path: "/", name: "홈" },
+      { path: "/life", name: "라이프" },
+      { path: "/food", name: "푸드" },
+      { path: "/travel", name: "여행" },
+      { path: "/culture", name: "컬쳐" },
+    ];
+
     return `
       <div class="first-row">
         <div class="container">
-        <span class="logo">ZUM 허브</span>
+        <span class="logo" route="/">ZUM 허브</span>
         <button class="bookmark">즐겨찾기</button>
         </div>
       </div>
-      <nav class="second-row">
-        <ul class="button-list">
-          <li>
-            <button route="/content/life">라이프</button>
-          </li>
-          <li>
-            <button route="/content/food">푸드</button>
-          </li>
-          <li>
-            <button route="/content/travel">여행</button>
-          </li>
-          <li>
-            <button route="/content/culture">컬쳐</button>
-          </li>
-        </ul>
-      </nav>
-      `;
+      <nav class="second-row" data-component="nav-list"></nav
+    `;
+  }
+
+  mounted() {
+    const $navList = this.target.querySelector('[data-component="nav-list"]');
+
+    new NavList($navList, {
+      navItems: [
+        { path: "/", name: "홈" },
+        { path: "/life", name: "라이프" },
+        { path: "/food", name: "푸드" },
+        { path: "/travel", name: "여행" },
+        { path: "/culture", name: "컬쳐" },
+      ],
+    });
   }
 }
